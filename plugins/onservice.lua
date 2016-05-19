4b607c21003196cb88d08c4ee2b95b0eeb77c2f1
MrTorento <seyedalikia27@gmail.com>
2016-05-19 18:04:29 +0430

    Update onservice.lua

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -5,12 +5,7 @@ local bot_id = 186820339 -- your bot id
    -- like local bot_id = 1234567
     if matches[1] == 'leave' and is_admin(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
-    elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
-      --send_large_msg("chat#id"..msg.to.id, '', ok_cb, false)
-      -- chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
-      -- block_user("user#id"..msg.from.id,ok_cb,false)
     end
-end
  
 return {
   patterns = {
