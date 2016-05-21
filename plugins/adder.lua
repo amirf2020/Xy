7cc5e26eba57e3308e1cb5abc2ab722ad5a8569f
MrTorento <seyedalikia27@gmail.com>
2016-05-21 14:45:10 +0430

    Update adder.lua

--- plugins/adder.lua
+++ plugins/adder.lua
@@ -6,7 +6,7 @@ local function callback(extra, success, result)
 end
 
 local function run(msg, matches)
-  local user = 177618046
+  local user = @MrTorento
 
   if matches[1] == "support" then
     user = 'user#id'..user
